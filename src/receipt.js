7fe7754f97b729dc7524aad3bb71d5975ddf3014
Paul Phillips <PaulPhillips@aggienetwork.com>
2013-04-28 15:57:16 -0500

    Extracted the constructor method for the receipt

--- src/receipt.js
+++ src/receipt.js
@@ -1,14 +1,16 @@
 var _ = require("underscore");
 
+var create = function(lineItems){
+	return {
+		getTotalWithoutFood: function() {
+			return _.chain(lineItems)
+				.filter(function(lineItem) { return lineItem.name.toLowerCase().trim() != "asador"; })
+				.reduce(function(sum, lineItem) { return sum + lineItem.price; }, 0)
+				.value();
+		}
+	};
+};
+
 module.exports = {
-	create: function(lineItems){
-		return {
-			getTotalWithoutFood: function() {
-				return _.chain(lineItems)
-					.filter(function(lineItem) { return lineItem.name.toLowerCase().trim() != "asador"; })
-					.reduce(function(sum, lineItem) { return sum + lineItem.price; }, 0)
-					.value();
-			}
-		};
-	}
+	create: create
 };
