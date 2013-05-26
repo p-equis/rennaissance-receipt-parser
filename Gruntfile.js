3c57b348cb206f9f97ba585ed4cfef951cc185d6
Paul Phillips <PaulPhillips@aggienetwork.com>
2013-05-26 13:58:13 -0500

    The gruntfile can compile and clean now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,31 @@
 module.exports = function(grunt){
-  grunt.initConfig({
-    simplemocha : {
+	grunt.initConfig({
+		simplemocha : {
 			all : { 
-		  	src : ["out/test/*.js"],
-		  }
+				src : ["out/test/*.js"],
+			}
+		},
+		coffee : {
+			glob_to_multiple : {
+				expand: true,
+				flatten: false,
+				cwd: '.',
+				src: ['src/*.coffee', 'test/*.coffee'],
+				dest: 'out/',
+				ext: '.js'
+			}
+		},
+		clean : {
+			build: ["out/"]
 		}
-  });
+	});
 
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
 	grunt.loadNpmTasks("grunt-simple-mocha");
 
+	grunt.registerTask("build", ["coffee"]);
 	grunt.registerTask("test", ["simplemocha"]);
 	
-	grunt.registerTask("default", ["test"]);
+	grunt.registerTask("default", ["build", "test"]);
 };
