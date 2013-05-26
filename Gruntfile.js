c5e831c100f3bb8c2f6381394881c8a6ab3a77f3
Paul Phillips <PaulPhillips@aggienetwork.com>
2013-05-26 17:51:01 -0500

    Linting and changed the reporter showing up for mocha tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,29 +3,42 @@ module.exports = function(grunt){
 		simplemocha : {
 			all : { 
 				src : ["out/test/*.js"],
+			},
+			options : {
+				reporter : 'progress'
 			}
 		},
 		coffee : {
 			glob_to_multiple : {
 				expand: true,
 				flatten: false,
-				cwd: '.',
-				src: ['src/*.coffee', 'test/*.coffee'],
-				dest: 'out/',
-				ext: '.js'
+				cwd: ".",
+				src: ["src/*.coffee", "test/*.coffee"],
+				dest: "out/",
+				ext: ".js"
 			}
 		},
 		clean : {
 			build: ["out/"]
+		},
+		coffeelint : {
+			app : ["src/*.coffee", "test/*.coffee"],
+			options : {
+				no_tabs: {level: "ignore"},
+				indentation : {level: "ignore"},
+				max_line_length : {level: "ignore"},
+				no_trailing_whitespace : {level: "ignore"}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-clean");
 	grunt.loadNpmTasks("grunt-contrib-coffee");
 	grunt.loadNpmTasks("grunt-simple-mocha");
+	grunt.loadNpmTasks("grunt-coffeelint");
 
 	grunt.registerTask("build", ["coffee"]);
 	grunt.registerTask("test", ["simplemocha"]);
 	
-	grunt.registerTask("default", ["build", "test"]);
+	grunt.registerTask("default", ["coffeelint", "build", "test"]);
 };
